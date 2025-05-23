cc7f22facf28de3f8571849926112538b08a96b1
Viswaa RKK <vr2023@hw.ac.uk>
2025-05-23 21:35:22 +0200

    Initial commit

---
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -0,0 +1,29 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class Main extends ApplicationAdapter
+{
+    @Override
+    public void create()
+    {
+
+    }
+
+    @Override
+    public void render()
+    {
+
+    }
+
+    @Override
+    public void dispose()
+    {
+        //hi there!
+    }
+}
