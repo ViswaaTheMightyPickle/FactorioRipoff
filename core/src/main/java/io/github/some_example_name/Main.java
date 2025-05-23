531f550048725cb3bc4ca29957bcc3c44cd468aa
Viswaa RKK <vr2023@hw.ac.uk>
2025-05-23 21:55:42 +0200

    Drawing first sample texture!
    I've used the sprite batch and texture objects to first test whether drawing calls work! The OpenGL learning curve exists, but this framework makes things much MUCH simpler.

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -9,21 +9,32 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter
 {
+
+    Texture grassTexture;
+    SpriteBatch batch;
+
     @Override
     public void create()
     {
-
+        grassTexture = new Texture(("temp_grass_texture.png"));
+        batch = new SpriteBatch();
     }
 
     @Override
     public void render()
     {
-
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //clearing the openGl buffer so we don't have weird smearing artefacts.
+        //Let's begin drawing!
+        batch.begin();
+        batch.draw(grassTexture,0,0, 200, 200); //first we specify the texture, then the position.
+        // then we specify the scale of the texture!
+        batch.end();
     }
 
     @Override
     public void dispose()
     {
-        
+        grassTexture.dispose();
+        batch.dispose();
     }
 }
